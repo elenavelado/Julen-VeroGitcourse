b721869f5b8ffcf3ba8a8110c4f3e24d8018b628
elenavelado <elena.velado@uah.es>
2021-10-18 13:23:39 +0200

    comandos Git

--- Ejercicios.R
+++ Ejercicios.R
@@ -1,2 +1,9 @@
 #18/10/2021
 #Exercise 3:First Push 
+
+#Comandos Git:
+#git restore: deshace un git add y/o los cambios del directorio de trabajo.
+#git reset --soft HEAD~1: deshace el último commit pero no el add y los cambios realizados en el directorio de trabajo.
+#git reset --mixed HEAD~1: deshace el add y el commit pero no los cambios realizados en el directorio de trabajo.
+#git reset --hard HEAD~1: deshace el add, el commit y todos los cambios realizados en el directorio de trabajo.
+#git revert HEAD: es la opción segura de git reset para deshacer un commit ya que no resetea un proyecto a su estado anterior eliminando todos los commits posteriores (es decir, no elimina el historial de commits).
